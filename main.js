c88d00a87935c34a8ce2f79c96aec5e9fbb9cdf8
ARBOX128 <118316306+ARBOX128@users.noreply.github.com>
2023-07-17 16:38:59 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("mi primer proyector e la consola");
+
+var primer_nombre = "matias"
+var apellido = "perez";
+var nombre_completo = primer_nombre.concat(apellido);
+console.log(nombre_completo);
+
+var a = 10;
+var b = 5;
+var restar = a - b;
+console.log(restar);
+
+var sumar = a + b;
+console.log(sumar);
+
+var multiplicar = a * b;
+console.log(multiplicar);
+var dividir = a / b;
+console.log(dividir);
